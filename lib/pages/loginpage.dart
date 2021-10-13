ce9de91de7c6d0d1afb101ffa2541d14dc6cc325
rahatkushtia <imranulislamrahat@gmail.com>
2021-10-13 15:53:35 +0600

    day4 complete

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -9,12 +9,46 @@ class LoginPage extends StatelessWidget {
       color: Colors.white,
       child: Column(
         children: [
-          Image.asset("assets/images/01image.png"),
+          SizedBox(
+              width: 400,
+              height: 300,
+              child: Image.asset("assets/images/01image.png",
+                  fit: BoxFit.contain)),
+          const SizedBox(
+            height: 60,
+          ),
           const Text(
-            "Well Come",
+            "Login Page",
             style: TextStyle(
                 fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue),
-          )
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 8.0, horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "Enter Your User Id",
+                    labelText: "User Id",
+                  ),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "Enter Your PassWord",
+                    labelText: "PassWord",
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text("Forgot Password"),
         ],
       ),
     );
