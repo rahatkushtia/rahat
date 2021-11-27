ea037201c3361d9f3d584e39f9fd734ad493740b
rahatkushtia <imranulislamrahat@gmail.com>
2021-11-27 14:52:59 +0600

    day 17 completed

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -5,8 +5,21 @@ class ProFile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('This Is Profile'),
+    return Material(
+      child: SafeArea(
+          child: Column(
+        children: [
+          Container(
+            width: 150,
+            height: 150,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(100.0),
+              child: Image.network(
+                  "https://pbs.twimg.com/profile_images/1463071608281518086/jYQxqvj9_400x400.jpg"),
+            ),
+          )
+        ],
+      )),
     );
   }
 }
