be17ef4c25cc4a94d0a2759bfcd201c703bad363
rahatkushtia <imranulislamrahat@gmail.com>
2021-11-27 15:53:33 +0600

    day 18 completed

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -17,6 +17,28 @@ class ProFile extends StatelessWidget {
               child: Image.network(
                   "https://pbs.twimg.com/profile_images/1463071608281518086/jYQxqvj9_400x400.jpg"),
             ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(8.0)),
+            width: 400,
+            child: Card(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  child: Column(
+                    children: [
+                      Text(
+                        "Name: Sabit Ibn Imran",
+                        style: TextStyle(fontSize: 24),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
           )
         ],
       )),
